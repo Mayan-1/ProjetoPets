b72d4c519f291a89fd32efa0d1cb446b284bec79
Mayan <may.santana197@gmail.com>
2023-12-14 17:20:59 -0300

    Criando tela para realizar denuncias

---
+++ app/src/main/java/com/example/app/TelaCriarDenuncias.java
@@ -0,0 +1,14 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaCriarDenuncias extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_criar_denuncias);
+    }
+}
