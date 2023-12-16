6f6c45f9b66095faff8997fc3b8380ebcc96f126
Mayan <may.santana197@gmail.com>
2023-12-15 22:58:42 -0300

    Atualizando telas

--- app/src/main/java/com/example/app/TelaCriarDenuncias.java
+++ app/src/main/java/com/example/app/TelaCriarDenuncias.java
@@ -1,14 +1,62 @@
 package com.example.app;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class TelaCriarDenuncias extends AppCompatActivity {
 
+    private Uri uriImagem;
+    private Button btnAnexarImagem;
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private ImageView ivImagemDenuncia;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_criar_denuncias);
+
+        ivImagemDenuncia = findViewById(R.id.iv_image_denuncia);
+        btnAnexarImagem = findViewById(R.id.btn_imagem);
+
+        btnAnexarImagem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirGaleria();
+            }
+        });
+    }
+
+    private void abrirGaleria(){
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(Intent.createChooser(intent, "Escolha uma imagem"), 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK){
+
+            if (requestCode == 0){
+
+                if (data != null){
+
+                    uriImagem = data.getData();
+
+
+                }
+            }
+        }
+
     }
 }
